4832958014b0933492e0692112939f109cb204ae
endlmk <1269467+endlmk@users.noreply.github.com>
2024-02-03 17:18:12 +0900

    Add ClosedRange class.

--- ClosedRage.R
+++ ClosedRage.R
@@ -1,1 +1,6 @@
-testthat::expect_equal(1 + 2, 3)
+setClass("ClosedRange", slots = list(start = "numeric", end = "numeric"))
+
+testthat::test_that("Can create ClosedRange object", {
+  closed_range <- new("ClosedRange", start = 1, end = 2)
+  testthat::expect_true(!is.null(closed_range))
+})
