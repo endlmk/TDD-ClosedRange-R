6b0bef50e7462ccc66cca737742dc3b3030745c0
endlmk <1269467+endlmk@users.noreply.github.com>
2024-02-04 18:20:54 +0900

    Implement include range.

--- ClosedRange.R
+++ ClosedRange.R
@@ -15,15 +15,29 @@ setMethod("as.character", "ClosedRange", function(x, ...) {
 
 setGeneric("contains", function(r, x) standardGeneric("contains"))
 
-setMethod("contains", "ClosedRange", function(r, x) {
-  r@start <= x && x <= r@end
-})
+setMethod(
+  "contains",
+  signature(r = "ClosedRange", x = "integer"),
+  function(r, x) {
+    r@start <= x && x <= r@end
+  }
+)
 
 setMethod(
   "Compare",
   signature(e1 = "ClosedRange", e2 = "ClosedRange"),
   function(e1, e2) {
     e1@start == e2@start && e1@end == e2@end
+  }
+)
+
+setGeneric("includes", function(r1, r2) standardGeneric("includes"))
+
+setMethod(
+  "includes",
+  signature(r1 = "ClosedRange", r2 = "ClosedRange"),
+  function(r1, r2) {
+    r1@start <= r2@start && r2@end <= r1@end
   }
 )
 
@@ -48,12 +62,12 @@ testthat::test_that("Can show range", {
 
 testthat::test_that("Value is in range", {
   cr <- new("ClosedRange", start = 1L, end = 2L)
-  testthat::expect_true(contains(cr, 1))
+  testthat::expect_true(contains(cr, 1L))
 })
 
 testthat::test_that("Value is out of range", {
   cr <- new("ClosedRange", start = 1L, end = 2L)
-  testthat::expect_false(contains(cr, 3))
+  testthat::expect_false(contains(cr, 3L))
 })
 
 testthat::test_that("Equal range", {
@@ -66,4 +80,22 @@ testthat::test_that("Not equal range", {
   cr1 <- new("ClosedRange", start = 1L, end = 2L)
   cr2 <- new("ClosedRange", start = 1L, end = 3L)
   testthat::expect_false(cr1 == cr2)
+})
+
+testthat::test_that("Include range", {
+  cr1 <- new("ClosedRange", start = 1L, end = 2L)
+  cr2 <- new("ClosedRange", start = 1L, end = 2L)
+  testthat::expect_true(includes(cr1, cr2))
+})
+
+testthat::test_that("Not include range1", {
+  cr1 <- new("ClosedRange", start = 1L, end = 2L)
+  cr2 <- new("ClosedRange", start = 1L, end = 3L)
+  testthat::expect_false(includes(cr1, cr2))
+})
+
+testthat::test_that("Not include range2", {
+  cr1 <- new("ClosedRange", start = 1L, end = 2L)
+  cr2 <- new("ClosedRange", start = 0L, end = 1L)
+  testthat::expect_false(includes(cr1, cr2))
 })
